5bebef2a10b7fedbee9adf18c0ebed1161f1724a
swatantrajha <swatantrajha7>
2020-05-29 14:15:18 +0530

    this commit refs datatables changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,20 +11,16 @@ require("jquery")
 //= require bootstrap
 //= require datatables
 //= require jquery
+//= require datatables
 //= require jquery_ujs
 
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-window.jQuery = require('jquery')
 $(document).ready(function() {
   $(".dataTables").dataTable({
-    paging: false,
-    searching: false,
-    bInfo : false,
+    "iDisplayLength": 50,
+    responsive: true
   });
 });
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,10 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-environment.plugins.append(
-    'Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-    })
-)
+module.exports = environment.toWebpackConfig()
